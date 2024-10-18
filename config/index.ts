5edc1815223197cf1528dbf3dff2c72808fdf306
Yasuhiro Yamanaka <yasdaq.y@gmail.com>
2024-10-18 21:46:43 +0900

    env variable updated

--- config/index.ts
+++ config/index.ts
@@ -1,13 +1,13 @@
 import type { AppInfo } from '@/types/app'
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const API_KEY = `${process.env.APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Mendo Remove Mondory',
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'jp',
+  default_language: 'ja',
 }
 
 export const isShowPrompt = false
